be37fde0a55a5a321a81aa7d38166237ec314612
Elias Bader <elias.bader@fh-salzburg.ac.at>
2024-02-06 14:57:55 +0100

    more template data for backend

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -10,18 +10,34 @@ def panels():
     return {
         "panels": [
             {
+                'id': 1,
                 'name': 'Panel 1',
                 'currentW': 200,
                 'dayYieldW': 1000,
                 'totalYieldW': 10000
 
+            },
+            {
+                'id': 2,
+                'name': 'Panel 2',
+                'currentW': 250,
+                'dayYieldW': 1400,
+                'totalYieldW': 130000
+
+            },
+            {
+                'id': 3,
+                'name': 'Panel 3',
+                'currentW': 150,
+                'dayYieldW': 600,
+                'totalYieldW': 7000
             }
         ]
     }
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return '<div><h3>ROUTES:</h3> <p>GET: <a href="/api/panels">/api/panels</a><p></div>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
