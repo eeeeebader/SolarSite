e11cf8b212e6fbf0e30310a5562ab336741c639b
Elias Bader <elias.bader@fh-salzburg.ac.at>
2024-02-18 21:54:18 +0100

    Update weather service endpoint to use environment variable

--- Frontend/src/app/weather.service.ts
+++ Frontend/src/app/weather.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { WeatherData } from './weather.interface';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  private endpoint = 'https://api.openweathermap.org/data/2.5/weather?lat=47.93972794656145&lon=12.848657792315324&appid=22101392412f91b14a1906d7dcb03bd5&units=metric';
+  private endpoint = 'https://api.openweathermap.org/data/2.5/weather?lat=47.93972794656145&lon=12.848657792315324&appid=' + environment.weatherAPIKey + '22101392412f91b14a1906d7dcb03bd5&units=metric';
 
   constructor(private http: HttpClient) {}
 
